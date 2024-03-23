78814a20f51ad9544efbe9c130efde3b35d56437
Sodasom <1219dasom@gamil.com>
2024-03-23 14:16:46 +0900

    Init: 하루사리  프로젝트 초기 세팅

---
+++ src/routes/router.tsx
@@ -0,0 +1,21 @@
+import { createBrowserRouter } from "react-router-dom";
+
+import App from "../App";
+import MainPage from "../pages/main";
+import ListPage from "../pages/list";
+import WritePage from "../pages/write";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      { index: true, element: <MainPage /> },
+      { path: "/", element: <MainPage /> },
+      { path: "/list", element: <ListPage /> },
+      { path: "/write", element: <WritePage /> },
+    ],
+  },
+]);
+
+export default router;
