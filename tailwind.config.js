3ac1891a836c0dac64d57bf8e9fe9d94c6b8ef6e
Sodasom <1219dasom@gamil.com>
2024-03-30 09:38:36 +0900

    🎨 Design: color theme 설정

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,26 @@
 export default {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          point: "#52B69A",
+        },
+        base: {
+          white: "#FFFFFF",
+          gray10: "#E0E0E0",
+          gray20: "#C7C7C7",
+          gray30: "#AEAEAE",
+          gray40: "#959595",
+          gray50: "#777677",
+          gray60: "#5A5A5A",
+          gray70: "#464646",
+          gray80: "#323232",
+          gray90: "#121212",
+          black: "#000000",
+        },
+      },
+    },
   },
   plugins: [],
 };
